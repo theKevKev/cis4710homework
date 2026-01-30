497fcfe75fce36e2f15b738623ddaf7109565a35
theKevKev <kevinhan.119@gmail.com>
2026-01-30 03:37:21 +0000

    homework 2a complete: divider unsigned

--- hw2a-divider/DividerUnsigned.sv
+++ hw2a-divider/DividerUnsigned.sv
@@ -1,4 +1,7 @@
-/* INSERT NAME AND PENNKEY HERE */
+/* Authors: 
+ * Kevin Han kevinhan
+ * Gaurav Goel gg8
+ */
 
 `timescale 1ns / 1ns
 
@@ -11,7 +14,42 @@ module DividerUnsigned (
     output wire [31:0] o_quotient
 );
 
-    // TODO: your code here
+  wire [31:0] dividend [32];
+  wire [31:0] remainder[31];
+  wire [31:0] quotient [31];
+
+  DividerOneIter first_divide_iter (
+      .i_dividend (i_dividend),
+      .i_divisor  (i_divisor),
+      .i_remainder(32'b0),
+      .i_quotient (32'b0),
+      .o_dividend (dividend[0]),
+      .o_remainder(remainder[0]),
+      .o_quotient (quotient[0])
+  );
+
+  genvar i;
+  for (i = 1; i < 31; i = i + 1) begin
+    DividerOneIter intermediate_divide_iters (
+        .i_dividend (dividend[i-1]),
+        .i_divisor  (i_divisor),
+        .i_remainder(remainder[i-1]),
+        .i_quotient (quotient[i-1]),
+        .o_dividend (dividend[i]),
+        .o_remainder(remainder[i]),
+        .o_quotient (quotient[i])
+    );
+  end
+
+  DividerOneIter final_divide_iter (
+      .i_dividend (dividend[30]),
+      .i_divisor  (i_divisor),
+      .i_remainder(remainder[30]),
+      .i_quotient (quotient[30]),
+      .o_dividend (dividend[31]),
+      .o_remainder(o_remainder),
+      .o_quotient (o_quotient)
+  );
 
 endmodule
 
@@ -38,6 +76,20 @@ module DividerOneIter (
     }
     */
 
-    // TODO: your code here
+  assign o_dividend = i_dividend << 1;
+  logic [31:0] remainder_int, o_quotient_logic, o_remainder_logic;
+  assign remainder_int = i_dividend >> 31 & 32'b1 | i_remainder << 1;
 
+  always_comb begin
+    if (remainder_int < i_divisor) begin
+      o_quotient_logic  = i_quotient << 1;
+      o_remainder_logic = remainder_int;
+    end else begin
+      o_quotient_logic  = i_quotient << 1 | 32'b1;
+      o_remainder_logic = remainder_int - i_divisor;
+    end
+  end
+
+  assign o_quotient  = o_quotient_logic;
+  assign o_remainder = o_remainder_logic;
 endmodule
