6ce2d6f51d28d54451c77907e441494967856cfa
gg8git <gg40320vlogs@gmail.com>
2026-02-10 22:39:55 -0500

    hw2b

--- hw2b-cla/CarryLookaheadAdder.sv
+++ hw2b-cla/CarryLookaheadAdder.sv
@@ -26,7 +26,12 @@ module gp4(input wire [3:0] gin, pin,
            output wire gout, pout,
            output wire [2:0] cout);
 
-   // TODO: your code here
+   assign cout[0] = gin[0] | (pin[0] & cin);
+   assign cout[1] = gin[1] | (pin[1] & gin[0]) | (pin[1] & pin[0] & cin);
+   assign cout[2] = gin[2] | (pin[2] & gin[1]) | (pin[2] & pin[1] & gin[0]) | (pin[2] & pin[1] & pin[0] & cin);
+
+   assign gout = gin[3] | (pin[3] & gin[2]) | (pin[3] & pin[2] & gin[1]) | (pin[3] & pin[2] & pin[1] & gin[0]);
+   assign pout = (pin[3] & pin[2] & pin[1] & pin[0]);
 
 endmodule
 
@@ -36,7 +41,16 @@ module gp8(input wire [7:0] gin, pin,
            output wire gout, pout,
            output wire [6:0] cout);
 
-   // TODO: your code here
+   assign cout[0] = gin[0] | (pin[0] & cin);
+   assign cout[1] = gin[1] | (pin[1] & gin[0]) | (pin[1] & pin[0] & cin);
+   assign cout[2] = gin[2] | (pin[2] & gin[1]) | (pin[2] & pin[1] & gin[0]) | (pin[2] & pin[1] & pin[0] & cin);
+   assign cout[3] = gin[3] | (pin[3] & gin[2]) | (pin[3] & pin[2] & gin[1]) | (pin[3] & pin[2] & pin[1] & gin[0]) | (pin[3] & pin[2] & pin[1] & pin[0] & cin);
+   assign cout[4] = gin[4] | (pin[4] & gin[3]) | (pin[4] & pin[3] & gin[2]) | (pin[4] & pin[3] & pin[2] & gin[1]) | (pin[4] & pin[3] & pin[2] & pin[1] & gin[0]) | (pin[4] & pin[3] & pin[2] & pin[1] & pin[0] & cin);
+   assign cout[5] = gin[5] | (pin[5] & gin[4]) | (pin[5] & pin[4] & gin[3]) | (pin[5] & pin[4] & pin[3] & gin[2]) | (pin[5] & pin[4] & pin[3] & pin[2] & gin[1]) | (pin[5] & pin[4] & pin[3] & pin[2] & pin[1] & gin[0]) | (pin[5] & pin[4] & pin[3] & pin[2] & pin[1] & pin[0] & cin);
+   assign cout[6] = gin[6] | (pin[6] & gin[5]) | (pin[6] & pin[5] & gin[4]) | (pin[6] & pin[5] & pin[4] & gin[3]) | (pin[6] & pin[5] & pin[4] & pin[3] & gin[2]) | (pin[6] & pin[5] & pin[4] & pin[3] & pin[2] & gin[1]) | (pin[6] & pin[5] & pin[4] & pin[3] & pin[2] & pin[1] & gin[0]) | (pin[6] & pin[5] & pin[4] & pin[3] & pin[2] & pin[1] & pin[0] & cin);
+
+   assign gout = gin[7] | (pin[7] & gin[6]) | (pin[7] & pin[6] & gin[5]) | (pin[7] & pin[6] & pin[5] & gin[4]) | (pin[7] & pin[6] & pin[5] & pin[4] & gin[3]) | (pin[7] & pin[6] & pin[5] & pin[4] & pin[3] & gin[2]) | (pin[7] & pin[6] & pin[5] & pin[4] & pin[3] & pin[2] & gin[1]) | (pin[7] & pin[6] & pin[5] & pin[4] & pin[3] & pin[2] & pin[1] & gin[0]);
+   assign pout = (pin[7] & pin[6] & pin[5] & pin[4] & pin[3] & pin[2] & pin[1] & pin[0]);
 
 endmodule
 
@@ -45,6 +59,62 @@ module CarryLookaheadAdder
    input wire         cin,
    output wire [31:0] sum);
 
-   // TODO: your code here
+   wire [31:0] cout;
+   wire [31:0] g1, p1;
+   wire [8:0] g2, p2;
+   wire gout, pout;
+
+   genvar i;
+   generate
+   for (i = 0; i < 32; i = i+1) begin
+      gp1 first(
+         .a(a[i]),
+         .b(b[i]),
+         .g(g1[i]),
+         .p(p1[i])
+      );
+   end
+   endgenerate
+
+   gp4 top(
+      .gin(g1[3:0]),
+      .pin(p1[3:0]),
+      .cin(cin),
+      .gout(g2[0]),
+      .pout(p2[0]),
+      .cout(cout[2:0])
+   );
+
+   generate
+      for (i = 1; i < 8; i = i+1) begin
+         gp4 bottoms(
+            .gin(g1[(4*i+3):(4*i)]),
+            .pin(p1[(4*i+3):(4*i)]),
+            .cin(cout[4*i-1]),
+            .gout(g2[i]),
+            .pout(p2[i]),
+            .cout(cout[(4*i+2):(4*i)])
+         );
+      end
+   endgenerate
+
+   gp8 last(
+      .gin(g2[7:0]),
+      .pin(p2[7:0]),
+      .cin(cin),
+      .gout(gout),
+      .pout(pout),
+      .cout({cout[27], cout[23], cout[19], cout[15], cout[11], cout[7], cout[3]})
+   );
+
+   assign cout[31] = gout | (pout & cin);
+
+   assign sum[0] = (a[0] ^ b[0]) ^ cin;
+
+   generate
+      for (i = 1; i < 32; i = i+1) begin
+         assign sum[i] = (a[i] ^ b[i]) ^ cout[i-1];
+      end
+   endgenerate
 
 endmodule
